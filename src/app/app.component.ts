e16f844957770f5b3d93fbea21352e49d9e592c2
Linus Gasser <linus.gasser@epfl.ch>
2020-05-28 20:13:17 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {KeyPair, User} from "@c4dt/dynacred";
+import {Log} from "@dedis/cothority";
+import {LocalCache} from "@dedis/cothority/byzcoin/byzcoin-rpc";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-dynacred';
+export class AppComponent implements OnInit {
+    title = 'test-dynacred';
+
+    async ngOnInit() {
+        const kp = KeyPair.rand();
+        Log.print(kp.toString());
+        await User.migrate(new LocalCache());
+    }
 }
